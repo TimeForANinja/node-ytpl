74cadee3d0e93827b01259a00e1df6c8c4fd62be
Halasi Tamás <trusted.tomato@gmail.com>
2017-06-06 05:53:51 +0200

    Add TypeScript definitions (#3)
    * Add typings
    * Add .gitignore

---
+++ typings/index.d.ts
@@ -0,0 +1,45 @@
+declare module 'ytpl' {
+	namespace ytpl{
+		type options = {
+			/** Limits the pulled items. */
+			limit?:number
+		}
+		type result = {
+			id:string
+			url:string
+			title:string
+			visibility:string
+			description:string
+			views:string
+			last_updated:string
+			author:{
+				id:string
+				name:string
+				avatar:string
+				user:string
+				channel_url:string
+				user_url:string
+			}
+			items:{
+				id:string
+				url:string
+				url_simple:string
+				title:string
+				thumbnail:string
+				duration:string
+				author:{
+					id:string
+					name:string
+					user:string
+					channel_url:string
+					user_url:string
+				}
+			}[]
+		}
+	}
+	function ytpl(id:string,callback:((err:Error,result:ytpl.result)=>any)):void
+	function ytpl(id:string,options:ytpl.options,callback:((err:Error,result:ytpl.result)=>any)):void
+	function ytpl(id:string):Promise<ytpl.result>
+	function ytpl(id:string,options:ytpl.options):Promise<ytpl.result>
+	export = ytpl;
+}
