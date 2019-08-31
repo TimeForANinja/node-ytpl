b9f83999f95e055ca3ad161e5e2441b6dc49796a
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-08-31 23:31:11 +0200

    add validateURL

--- test/main-test.js
+++ test/main-test.js
@@ -87,3 +87,31 @@ describe('main()', () => {
     });
   });
 });
+
+describe('validateURL()', () => {
+  it('recognises users', done => {
+    ASSERT.equal(YTPL.validateURL('www.youtube.com/user/someUser'), true);
+  })
+
+  it('recognises channels', done => {
+    ASSERT.equal(YTPL.validateURL('youtube.com/channel/UC01234567890ASDFGHJKL'), true);
+  })
+
+  it('recognises playlist links', done => {
+    ASSERT.equal(YTPL.validateURL('http://www.youtube.com/playlist?list=PL0123456789ASDFGHJK'), true);
+
+  })
+
+  it('recognises id\'s', done => {
+    ASSERT.equal(YTPL.validateURL('PL0123456789ASDFGHJK'), true);
+
+  })
+
+  it('fails for videos', done => {
+    ASSERT.equal(YTPL.validateURL('youtube.com/watch?v=asdf1234'), false);
+  })
+
+  it('fails for random strings', done => {
+    ASSERT.equal(YTPL.validateURL('asdfagasdas'), false);
+  })
+});
