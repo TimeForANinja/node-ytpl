85693b4d92c6a119f563cc4ffe95301827ae2962
TimeForANinja <t.kutscha@yahoo.de>
2020-11-26 11:14:20 +0100

    update example & add update script

---
+++ example/update_examples.js
@@ -0,0 +1,13 @@
+const YTPL = require('../');
+const FS = require('fs');
+const UTIL = require('util');
+
+const main = async() => {
+  let saveString;
+
+  // Save playlist
+  const search = await YTPL('PLRBp0Fe2GpglkzuspoGv-mu7B2ce9_0Fn', { limit: 15 });
+  saveString = UTIL.inspect(search, { depth: Infinity });
+  FS.writeFileSync('./example_output.txt', saveString);
+};
+main();
