11b2375d468bb25ed984f73f200d41aff6a8f7ea
TimeForANinja <t.kutscha@yahoo.de>
2020-12-11 02:53:07 +0100

    update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,37 +3,57 @@ declare module 'ytpl' {
     interface options {
       /** Limits the pulled items. */
       limit?: number;
-      headers?: { [key: string]: string; };
+      /** Limits the pulled pages - overwrites limit. */
+      pages?: number;
+      /** Request Options for Miniget */
+      requestOptions?: { headers?: { [key: string]: string; }};
+    }
+    interface image {
+      url: string;
+      width: number;
+      height: number;
+    }
+    interface item {
+      title: string;
+      index: number;
+      id: string;
+      shortUrl: string;
+      url: string;
+      author: {
+        name: string;
+        ref: string;
+        channelID: string;
+      };
+      thumbnails: image[];
+      bestThumbnail: image;
+      isLive: boolean;
+      duration: string | null;
+      durationSec: number | null;
     }
     interface result {
       id: string;
       url: string;
       title: string;
-      visibility: 'link only' | 'everyone';
+      estimated_items: number;
+      views: number;
+      thumbnails: image[],
+      bestThumbnail: image,
+      lastUpdated: string;
       description: string | null;
-      total_items: number;
-      views: string;
-      last_updated: string;
-      author: null | {
-        id: string;
+      visibility: 'unlisted' | 'everyone';
+      author: {
         name: string;
-        avatar: string;
-        user: string | null;
-        channel_url: string;
-        user_url: string | null;
+        ref: string;
+        avatars: image[];
+        bestAvatar: image;
+        channelID: string;
       };
-      items: {
-        id: string;
-        url: string;
-        url_simple: string;
-        title: string;
-        thumbnail: string;
-        duration: string | null;
-        author: null | {
-          name: string;
-          ref: string;
-        };
-      }[];
+      items: item[];
+      continuation: [string, string, object, object] | null;
+    }
+    interface continueResult {
+      continuation: [string, string, object, object] | null,
+      items: item[],
     }
 
     /**
@@ -47,12 +67,18 @@ declare module 'ytpl' {
      * @description Returns a promise that resovles to the playlist ID from a YouTube URL. Can be called with the playlist ID directly, in which case it returns it.
      */
     function getPlaylistID(link: string): Promise<string>;
+
+    /**
+     * @param continuationData Data provided from a previous request
+     * @description fetches one additional page & parses its items - only supported when using pages
+     */
+    function continueReq(continuationData: [string, string, object, object]): Promise<continueResult>;
   }
 
   /**
    * @description Attempts to resolve the given playlist id
    * @param id Can be the id of the YT playlist or playlist link or user link (resolves uploaded playlist) or channel link (resolves uploaded playlist)
-   * @param [options] Object with options. limit[Number] -> limits the pulled items, defaults to 100, set to Infinity to get the whole playlist
+   * @param [options] Object with options.
    * @returns Promise that resolves to playlist data;
    */
   function ytpl(id: string, options?: ytpl.options): Promise<ytpl.result>;
